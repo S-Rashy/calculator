1ea09edbeb30547f3a0c78114344558ed24e9b0c
S-Rashy <sadiqrashidah@gmail.com>
2024-09-10 17:27:07 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let showtext = document.getElementById("top");
+console.log(showtext.value);
+// showtext.value ='0'
+
+let addNum =(val)=>{
+    return showtext.textContent = showtext.value + val
+
+}
